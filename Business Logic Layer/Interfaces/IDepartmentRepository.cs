2af0929930af7c79594447207535b53a55e567b0
ziad <ziadalimohammed812@gmail.com>
2025-03-09 07:06:40 +0200

    Implementation Of Business Logic Layer

---
+++ Business Logic Layer/Interfaces/IDepartmentRepository.cs
@@ -0,0 +1,24 @@
+﻿using Data_Access_Layer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business_Logic_Layer.Interfaces
+{
+    public interface IDepartmentRepository
+    {
+        IEnumerable<Department> GetAll();
+
+        Department? Get(int id);
+
+        int Add(Department department);
+
+
+        int Update(Department department);
+
+
+        int Delete(Department department);
+    }
+}
