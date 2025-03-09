87c841a270df7253a1a0b762f639d615a2a4a941
ziad <ziadalimohammed812@gmail.com>
2025-03-09 06:26:56 +0200

    Implementation Of Data Acess Layer

---
+++ Data Access Layer/Data/Configurations/DepartmentConfigurations.cs
@@ -0,0 +1,19 @@
+﻿using Data_Access_Layer.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Access_Layer.Data.Configurations
+{
+    public class DepartmentConfigurations : IEntityTypeConfiguration<Department>
+    {
+        public void Configure(EntityTypeBuilder<Department> builder)
+        {
+            builder.Property(D => D.Id).UseIdentityColumn(10, 10);
+        }
+    }
+}
