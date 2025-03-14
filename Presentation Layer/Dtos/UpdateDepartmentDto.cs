9fbc874542b286a880f15f7ea31146179ab52944
ziad <ziadalimohammed812@gmail.com>
2025-03-14 02:27:41 +0200

    Refactoring Details & Edit & Delete Functions

---
+++ Presentation Layer/Dtos/UpdateDepartmentDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Presentation_Layer.Dtos
+{
+    public class UpdateDepartmentDto
+    {
+        [Required(ErrorMessage = "Code Is Required")]
+        public string Code { get; set; }
+
+        [Required(ErrorMessage = "Name Is Required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "CreateAt Is Required")]
+        public DateTime CreateAt { get; set; }
+    }
+}
