974ec2b1db1419eb9677b1f0927dd8dffce6a161
ziad <ziadalimohammed812@gmail.com>
2025-03-18 07:38:31 +0200

    Auto Mapper (Auto Mapping)

---
+++ Presentation Layer/Mapping/EmployeeProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Data_Access_Layer.Models;
+using Presentation_Layer.Dtos;
+
+namespace Presentation_Layer.Mapping
+{
+    public class EmployeeProfile : Profile
+    {
+        public EmployeeProfile()
+        {
+            CreateMap<CreateEmployeeDto, Employee>();
+            CreateMap<Employee, CreateEmployeeDto>();
+
+        }
+    }
+}
