a81501afdebcd0cc76ea1744175ffd8c1a3f0454
ziad <ziadalimohammed812@gmail.com>
2025-03-26 09:52:40 +0200

    Sign Out

---
+++ Presentation Layer/Controllers/AccountController.cs
@@ -0,0 +1,68 @@
+﻿using Data_Access_Layer.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Presentation_Layer.Dtos;
+
+namespace Presentation_Layer.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        public AccountController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        [HttpGet]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignUpDto model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user=await _userManager.FindByNameAsync(model.UserName);
+                if (user is null)
+                {
+                    user=await _userManager.FindByEmailAsync(model.Email);
+                    if(user is null)
+                    {
+                        user = new AppUser
+                        {
+                            UserName = model.UserName,
+                            FirstName = model.FirstName,
+                            LastName = model.LastName,
+                            Email = model.Email,
+                            IsAgree = model.IsAgree
+                        };
+
+                        var result = await _userManager.CreateAsync(user, model.Password);
+
+                        if (result.Succeeded)
+                        {
+                            return RedirectToAction("SignIn");
+                        }
+
+                        foreach (var error in result.Errors)
+                        {
+                            ModelState.AddModelError("", error.Description);
+                        }
+                    }
+
+                }
+
+                ModelState.AddModelError("", "Invalid SignUp !!");
+            }
+            return View(model);
+
+        }
+
+        [HttpGet]
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+    }
+}
