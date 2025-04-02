9c04be5bbb953e25da412cefa1e0706564b4e349
ziad <ziadalimohammed812@gmail.com>
2025-04-02 09:37:47 +0200

    User Controller

---
+++ Presentation Layer/Controllers/UserController.cs
@@ -0,0 +1,134 @@
+﻿using Data_Access_Layer.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Presentation_Layer.Dtos;
+using Presentation_Layer.Helpers;
+
+namespace Presentation_Layer.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public UserController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        public IActionResult Index(string? Search)
+        {
+            IEnumerable<UserToReturnDto> users;
+            if (Search is not null)
+            {
+                users = _userManager.Users.Select(U => new UserToReturnDto()
+                {
+                    Id = U.Id,
+                    UserName = U.UserName,
+                    FirstName = U.FirstName,
+                    LastName = U.LastName,
+                    Email = U.Email,
+                    Roles = _userManager.GetRolesAsync(U).Result
+
+                }).Where(U => U.FirstName.ToLower().Contains(Search.ToLower()));
+               
+            }
+            else
+            {
+                users = _userManager.Users.Select(U => new UserToReturnDto()
+                {
+                    Id = U.Id,
+                    UserName = U.UserName,
+                    FirstName = U.FirstName,
+                    LastName = U.LastName,
+                    Email = U.Email,
+                    Roles = _userManager.GetRolesAsync(U).Result
+
+                });
+               
+            }
+            return View(users);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(string? id, string ViewName = "Details")
+        {
+            if (id is null) return BadRequest("Invalid Id");
+
+            var user = await _userManager.FindByIdAsync(id);
+
+            if (user is null) return NotFound(new { StatusCode = 404, Message = $"User With Id {id} Is Not Found" });
+
+            var dto = new UserToReturnDto()
+            {
+                Id = user.Id,
+                UserName = user.UserName,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Email = user.Email,
+                Roles = _userManager.GetRolesAsync(user).Result
+            };
+            return View(ViewName, dto);
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(string? id)
+        {
+            
+            return await Details(id,"Edit");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit([FromRoute] string id, UserToReturnDto userDto)
+        {
+            if (ModelState.IsValid)
+            {
+                if(id != userDto.Id) return BadRequest("Invalid Operation");
+                var user = await _userManager.FindByIdAsync(id);
+
+                if (user is null) return BadRequest("Invalid Operation");
+
+                user.UserName = userDto.UserName;
+                user.FirstName = userDto.FirstName;
+                user.LastName = userDto.LastName;
+                user.Email = userDto.Email;
+
+                var result = await _userManager.UpdateAsync(user);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            return View(userDto);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(string? id)
+        {
+           
+            return await Details(id, "Delete");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete([FromRoute] string id, UserToReturnDto userDto)
+        {
+            if (ModelState.IsValid)
+            {
+
+                if (id != userDto.Id) return BadRequest("Invalid Operation");
+                var user = await _userManager.FindByIdAsync(id);
+
+                if (user is null) return BadRequest("Invalid Operation");
+
+                var result = await _userManager.DeleteAsync(user);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            return View(userDto);
+        }
+
+    }
+}
---
+++ Presentation Layer/Dtos/UserToReturnDto.cs
@@ -0,0 +1,19 @@
+﻿namespace Presentation_Layer.Dtos
+{
+    public class UserToReturnDto
+    {
+        public String Id { get; set; }
+
+        public String UserName { get; set; }
+
+        public String FirstName { get; set; }
+
+        public String LastName { get; set; }
+
+
+        public String Email { get; set; }
+
+
+        public IEnumerable<string>? Roles { get; set; }
+    }
+}
